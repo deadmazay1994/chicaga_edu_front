2d87c3039528f5119b42b6550ff7cc4867d63bd9
unknown <147rawil147@gmail.com>
2022-02-14 13:19:51 +0300

    добавил snuckbars

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <component :is="layout">
     <router-view />
+    <snuckbars />
   </component>
 </template>
 
 <script>
+import Snuckbars from "@/components/Base/Snackbars";
 export default {
   computed: {
     layout() {
       return this.$route.meta.layout || "default-layout";
     },
+  },
+  components: {
+    Snuckbars,
   },
 };
 </script>
