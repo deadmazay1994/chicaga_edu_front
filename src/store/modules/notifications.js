29eaac0c878349027d2188151be7438ec2e9f24d
unknown <147rawil147@gmail.com>
2022-02-18 12:15:52 +0300

    review

--- src/store/modules/notifications.js
+++ src/store/modules/notifications.js
@@ -3,31 +3,37 @@ export default {
   actions: {},
   mutations: {
     pushNotification(state, params) {
+      // Параметры по дефолту так сейчас не прописывают
+      // Можно присовить дефолтное значение прямо такой
+      // pushNotification(state, params = 5000) {
       if (!params.timeout) {
-        params.timeout = 5000
+        params.timeout = 5000;
       }
       state.notifications.push({
         ...params,
         active: true,
         pos: state.notifications.length,
-        state: params.state ? params.state : 'default' 
+        state: params.state ? params.state : "default",
       });
-      state.notifications.filter(elem => elem.active).reverse().forEach(element => {
-        setTimeout(() => {
-          element.active = false
-        }, params.timeout)
-      });
-    }
+      state.notifications
+        .filter((elem) => elem.active)
+        .reverse()
+        .forEach((element) => {
+          setTimeout(() => {
+            element.active = false;
+          }, params.timeout);
+        });
+    },
   },
   state: {
-    notifications: []
+    notifications: [],
   },
   getters: {
     getNotifications(state) {
-      return state.notifications
+      return state.notifications;
     },
     getActiveNotifications(state) {
-      return state.notifications.filter(not => not.active)
-    }
-  }
-}
+      return state.notifications.filter((not) => not.active);
+    },
+  },
+};
