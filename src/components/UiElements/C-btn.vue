4552f74e34d2dba39dee83fa3e4ca94d9daacfb6
Evgeny <hatber.index@gmail.com>
2022-02-18 17:31:19 +0900

    add global btn component

---
+++ src/components/UiElements/C-btn.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+  :class="btnClass"
+  :disabled="isDisabled"
+  @click="$emit('btn-event')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'C-btn',
+  props: {
+    btnClass: String,
+    isDisabled: Boolean
+  }
+}
+</script>
