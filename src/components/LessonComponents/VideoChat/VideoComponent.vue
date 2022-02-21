88b53f809fa75b78d1766339f806b91b3ce833dc
Evgeny <hatber.index@gmail.com>
2022-02-22 01:57:45 +0900

    убрана ошибка

--- src/components/LessonComponents/VideoChat/VideoComponent.vue
+++ src/components/LessonComponents/VideoChat/VideoComponent.vue
@@ -33,7 +33,6 @@
         @click.native="toggleFullSize"
         class="video-component__expand video-component__ctrls-btn"
         v-if="!active"
-        :
       />
       <expand
         v-else
