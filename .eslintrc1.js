6e1f9cefa7c21bd05e7b611802d6e71139aaaff0
unknown <147rawil147@gmail.com>
2022-02-24 12:52:09 +0300

    изменены настройки линтинга

--- .eslintrc1.js
+++ .eslintrc1.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["plugin:vue/recommended", "plugin:prettier-vue/recommended", "prettier"],
+  extends: [
+    "plugin:vue/recommended",
+    "plugin:prettier-vue/recommended",
+    "prettier",
+  ],
 
   settings: {
     "prettier-vue": {
@@ -76,7 +80,10 @@ module.exports = {
         singleQuote: false,
         semi: true,
         trailingComma: "es5",
-        arrowParens: "always",
+        arrowParens: "avoid",
+        bracketSpacing: true,
+        "space-before-function-paren": "never",
+        trailingComma: "none",
       },
     ],
   },
