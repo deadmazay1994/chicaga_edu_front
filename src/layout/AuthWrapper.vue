2c44201446d0af0e065673b63a3a789a5c021b05
Evgeny <hatber.index@gmail.com>
2022-03-09 19:47:37 +0900

    рефакторинг: все блоки sass в авторизации выстроены по иерархии

--- src/layout/AuthWrapper.vue
+++ src/layout/AuthWrapper.vue
@@ -85,53 +85,155 @@
       @media screen and (max-height: 450px)
         & 
           padding: .5rem 0
-      .auth-input
+      .auth-form__title
+        font-style: normal
+        font-weight: 700
+        font-size: 42px
+        line-height: 1.19em
         display: flex
         align-items: center
-        padding-bottom: 20px
-        border-bottom: 1.5px solid #fab5b4
-        width: 100%
-        margin-bottom: 30px
-        &::focus-within
-          border-color: red
-        &:last-child
-          margin-bottom: 0
+        color: red
+        margin-bottom: 5vh
         @media screen and (max-width: 1000px)
           &
-            margin-bottom: 2vh
-            padding-bottom: 2vh
-        svg
-          width: 22px
-          height: 22px
-          fill: red
-          margin-right: 15px
-        input
-          border: none
+            font-size: 24px
+            margin-bottom: 60px
+      .auth-form__header
+        display: flex
+        align-items: flex-end
+        margin-bottom: 5vh
+        justify-content: space-between
+        width: 100%
+        h2
+          margin-bottom: 0
+        .auth-form__step
+          font-family: Inter
           font-style: normal
-          font-weight: 500
-          font-size: 16px
-          line-height: 19px
-          color: #fab5b4
-          &:focus
+          font-weight: 700
+          font-size: 22px
+          line-height: 27px
+          color: red
+          span
+            font-size: 16px
+            font-weight: 300
+      .auth-form__body
+        display: flex
+        flex-direction: column
+        align-items: flex-start
+        width: 100%
+        position: relative
+        padding-bottom: 30px
+        @media screen and (max-height: 450px)
+          &
+            padding-bottom: 0
+        .auth-input
+          display: flex
+          align-items: center
+          padding-bottom: 20px
+          border-bottom: 1.5px solid #fab5b4
+          width: 100%
+          margin-bottom: 30px
+          &::focus-within
+            border-color: red
+          &:last-child
+            margin-bottom: 0
+          @media screen and (max-width: 1000px)
+            &
+              margin-bottom: 2vh
+              padding-bottom: 2vh
+          svg
+            width: 22px
+            height: 22px
+            fill: red
+            margin-right: 15px
+          input
+            border: none
+            font-style: normal
+            font-weight: 500
+            font-size: 16px
+            line-height: 19px
+            color: #fab5b4
+            &:focus
+              color: red
+            &:-ms-input-placeholder
+              color: #fab5b4
+            &::placeholder
+              color: #fab5b4
+        .reset-password-link
+          align-self: flex-end
+          margin-top: 5px
+        .red-msg
+          margin-bottom: 30px
+        .auth-msg
+          text-align: center
+          position: absolute
+          bottom: -17px
+          align-self: center
+          .red-text
+            bottom: -40px
+            color: #fab5b4
+      .auth-form__footer
+        display: flex
+        flex-direction: column
+        align-items: center
+        margin-top: 5vh
+        @media screen and (max-width:1000px)
+          &
+            margin-top: 2vh
+        .auth-form__submit
+          margin-bottom: 3vh
+        .auth-button
+          background: red
+          box-shadow: inset 3px 3px 15px 3px hsla(0, 0%, 100%, .5)
+          border-radius: 1.8em
+          padding: .9em
+          max-width: 315px
+          width: 100%
+          text-align: center
+          font-style: normal
+          font-weight: 700
+          font-size: 22px
+          line-height: 1.18em
+          color: #fff
+          @media screen and (max-width:1000px)
+            &
+              font-size: 16px
+              max-width: 200px
+          &:disabled
+            opacity: .3
+        .auth-form__link-bar
+          text-align: center
+          .arrow-link
+            font-style: normal
+            font-weight: 700
+            font-size: 14px
+            line-height: 17px
+            display: flex !important
+            align-items: center
             color: red
-          &:-ms-input-placeholder
-            color: #fab5b4
-          &::placeholder
-            color: #fab5b4
+          &:before
+            content: ""
+            width: 17px
+            height: 17px
+            background: url(~@/assets/imgs/icons/left-arrow.svg)
+            background-size: 100% 100%
+            display: none
+            margin-right: 10px
+          .auth-form__link
+            &:first-child
+              margin-right: 15px
+              display: inline-block
       .auth-label
         margin-bottom: 2vh
         font-weight: 700
+      .text-button
+        font-weight: 500
       .auth-label,
       .text-button
         font-style: normal
         font-size: 14px
         line-height: 17px
         color: red
-      .text-button
-        font-weight: 500
-      .reset-password-link
-        align-self: flex-end
-        margin-top: 5px
       .auth-checkbox
         display: flex
         -webkit-user-select: none
@@ -162,140 +264,25 @@
           border: 1px solid #fab5b4
           border-radius: 100%
           margin-right: 11px
-    .auth-form__title
-      margin-bottom: 5vh
-    .auth-form__header
-      display: flex
-      align-items: flex-end
-      margin-bottom: 5vh
-      justify-content: space-between
-      width: 100%
-      h2
-        margin-bottom: 0
-    .auth-form__body
-      display: flex
-      flex-direction: column
-      align-items: flex-start
-      width: 100%
-      position: relative
-      padding-bottom: 30px
-      @media screen and (max-height: 450px)
-        &
-          padding-bottom: 0
-      .red-msg
-        margin-bottom: 30px
       .auth-msg
-        text-align: center
-        position: absolute
-        bottom: -17px
-        align-self: center
-        .red-text
-          bottom: -40px
-          color: #fab5b4
-    .auth-form__footer
-      display: flex
-      flex-direction: column
-      align-items: center
-      margin-top: 5vh
-      @media screen and (max-width:1000px)
-        .auth-form__footer
-          margin-top: 2vh
-      .auth-form__submit
-        margin-bottom: 3vh
-      .auth-button
-        background: red
-        box-shadow: inset 3px 3px 15px 3px hsla(0, 0%, 100%, .5)
-        border-radius: 1.8em
-        padding: .9em
-        max-width: 315px
-        width: 100%
-        text-align: center
         font-style: normal
-        font-weight: 700
-        font-size: 22px
-        line-height: 1.18em
-        color: #fff
-        @media screen and (max-width:1000px)
-          &
-            font-size: 16px
-            max-width: 200px
-        &:disabled
-          opacity: .3
-    .auth-form__link-bar
-      text-align: center
-      .arrow-link
-        font-style: normal
-        font-weight: 700
+        font-weight: 500
         font-size: 14px
         line-height: 17px
-        display: flex !important
-        align-items: center
-        color: red
-      &:before
-        content: ""
-        width: 17px
-        height: 17px
-        background: url(~@/assets/imgs/icons/left-arrow.svg)
-        background-size: 100% 100%
-        display: none
-        margin-right: 10px
-    .auth-form__link
-      &:first-child
-        margin-right: 15px
-        display: inline-block
-    .auth-msg
-      font-style: normal
-      font-weight: 500
-      font-size: 14px
-      line-height: 17px
-      color: gray
-    .auth-form__step
-      font-family: Inter
-      font-style: normal
-      font-weight: 700
-      font-size: 22px
-      line-height: 27px
-      color: red
-      span
-        font-size: 16px
-        font-weight: 300
-    .auth-form__title
-      font-style: normal
-      font-weight: 700
-      font-size: 42px
-      line-height: 1.19em
-      display: flex
-      align-items: center
-      color: red
-      margin-bottom: 140px
-      @media screen and (max-width: 1000px)
-        &
-          font-size: 24px
-          margin-bottom: 60px
-
-.register-form
-  .auth-form__header
-    margin-bottom: 4vh
-  .auth-form__footer
-    margin-top: 100px
-
-.mb20
-  margin-bottom: 20px
-
-.mb50
-  margin-bottom: 50px
-
-.register-form .auth-form .auth-label
-  margin-bottom: 2vh
-
-@media screen and (max-width: 1000px)
-  .register-form .auth-form .auth-input
-    margin-bottom: 2vh
-    padding-bottom: 2vh
+        color: gray
+    .register-form
+      .auth-form
+        .auth-label
+          margin-bottom: 2vh
+        .auth-input
+          @media screen and (max-width: 1000px)
+            &
+              margin-bottom: 2vh
+              padding-bottom: 2vh
 </style>
 
 <style>
-/* @import "../assets/styles/style.min.css"; */
+@import "../assets/styles/style.min.css";
 </style>
 
 <script>
