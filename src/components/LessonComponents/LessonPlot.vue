7263d95613ffd2df8eb960a34d743a278a87dba2
Evgeny <hatber.index@gmail.com>
2022-03-17 19:25:27 +0900

    убран компонент доски рисования и добавлен новый с текстом 'coming soon'

---
+++ src/components/LessonComponents/LessonPlot.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lesson-plot">
+    <span>coming soon</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LessonPlot"
+}
+</script>
+
+<style lang="sass" scoped>
+.lesson-plot 
+  display: flex
+  align-items: center
+  justify-content: center
+  height: 100%
+  width: 100%
+  span
+    color: red
+</style>
