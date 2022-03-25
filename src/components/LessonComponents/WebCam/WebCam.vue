ee0903789c5cb679089aae77f4ba83f0e1a72ae3
Evgeny <hatber.index@gmail.com>
2022-03-25 19:29:57 +0900

    индикатор больше не горит при смене роута

--- src/components/LessonComponents/WebCam/WebCam.vue
+++ src/components/LessonComponents/WebCam/WebCam.vue
@@ -159,7 +159,9 @@ export default {
           this.showError("Доступ запрещен вашим устройством");
           break;
         case "DEVICE_ALREADY_IN_USE":
-          this.showError("Запрашиваемое устройство ввода занято другим процессом");
+          this.showError(
+            "Запрашиваемое устройство ввода занято другим процессом"
+          );
           break;
         case "SCREEN_CAPTURE_DENIED":
           this.showError("Демонстрация экрана была отклонена");
@@ -168,7 +170,9 @@ export default {
           this.showError("Ваш браузер не поддерживает демонстрацию экрана");
           break;
         case "SCREEN_EXTENSION_NOT_INSTALLED":
-          this.showError("Расширение для демонстрации экрана не было установлено");
+          this.showError(
+            "Расширение для демонстрации экрана не было установлено"
+          );
           break;
         case "SCREEN_EXTENSION_DISABLED":
           this.showError("Расширение для демонстрации экрана не включено");
@@ -180,13 +184,19 @@ export default {
           this.showError("Микрофон не был обнаружен");
           break;
         case "INPUT_AUDIO_DEVICE_GENERIC_ERROR":
-          this.showError("Произошла неизвестная ошибка при попытке доступа к аудиоустройству");
+          this.showError(
+            "Произошла неизвестная ошибка при попытке доступа к аудиоустройству"
+          );
           break;
         case "NO_INPUT_SOURCE_SET":
-          this.showError("Ошибка получения пользовательских медиаданных с устройств");
+          this.showError(
+            "Ошибка получения пользовательских медиаданных с устройств"
+          );
           break;
         case "PUBLISHER_PROPERTIES_ERROR":
-          this.showError("Не поддерживается частота кадров или разрешение экрана");
+          this.showError(
+            "Не поддерживается частота кадров или разрешение экрана"
+          );
           break;
         case "OPENVIDU_PERMISSION_DENIED":
           this.showError("Доступ запрещен");
@@ -304,6 +314,12 @@ export default {
     this.$on("publishScreen", () => {
       this.driver.publishScreen();
     });
+  },
+  watch: {
+    $route(to, from) {
+      this.driver.leaveSession();
+      this.$router.push(from.path);
+    }
   },
   created() {
     this.$watch(() => this.$route.params, this?.driver?.leaveSession);
