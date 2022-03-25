b890aab211541a51942e4eea2bcf984ed1b37fc2
Evgeny <hatber.index@gmail.com>
2022-03-25 16:26:08 +0900

    удален прошлый notification, добавлен vue-toastification

--- src/components/Base/Notifications.vue
+++ src/components/Base/Notifications.vue
@@ -1,31 +1,21 @@
 <template>
-  <snackbar
-    ref="snackbar"
-    baseSize="100px"
-    :wrapClass="'snackbar-wrapper'"
-    :colors="{open: '#333',info: '#3DBD7D',error: '#FA7377',warn: '#FF6600'}"
-    :holdTime="3000"
-    :multiple="true"
-    :position="'bottom-right'"
-  />
+  <div class="notification">
+    <button @click="test()">test</button>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import Snackbar from "vuejs-snackbar";
 
 export default {
   name: "Notifications",
   computed: {
     ...mapGetters(["snuckbars", "snuckbarsActive"])
   },
-  components: { Snackbar },
-  mounted() {
-    console.log(this.$refs.snackbar.info);
-    this.$refs.snackbar.info("msg");
+  methods: {
+    test() {
+      this.$toast("I'm a toast!");
+    }
   }
 };
 </script>
-
-<style lang="sass" scoped>
-</style>
