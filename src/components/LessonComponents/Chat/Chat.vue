8fa8761cf2aa981760e889c723ff72ef8ddd9d79
GizzaProgger <147rawil147@gmail.com>
2022-04-13 18:39:24 +0300

    установленно соеденение с чатом

--- src/components/LessonComponents/Chat/Chat.vue
+++ src/components/LessonComponents/Chat/Chat.vue
@@ -11,6 +11,7 @@ import TextareaChat from "./Textarea";
 import Masseges from "./Masseges";
 import HeaderChat from "./Header";
 import { mapGetters } from "vuex";
+import Driver from "rocketchat-client-api";
 
 export default {
   name: "chat",
@@ -30,13 +31,46 @@ export default {
   async mounted() {
     // Инициализация чата
     // Первым делом это!!!
-    await this.$store.dispatch("initChat", {
+    // await this.$store.dispatch("initChat", {
+    //   login: this.user.chat_auth_info.username,
+    //   password: this.user.chat_auth_info.password,
+    //   room: this.$route.params.id,
+    //   socketUrl: "wss://chat.edu.chicaga.ru/websocket/",
+    //   chatElem: this.$refs.chat
+    // });
+    let getUserInfo = chatId => {
+      chatId;
+      return {
+        name: "Sample user",
+        avatar:
+          "https://www.vokrug.tv/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg"
+      };
+    };
+    const hooks = {
+      async transformMsg(msg) {
+        let transformedMsg = msg;
+        let user = getUserInfo(msg.u._id);
+        transformedMsg.u.name = user.name;
+        transformedMsg.u.avatar = user.avatar;
+        return transformedMsg;
+      }
+    };
+    const driver = new Driver(hooks);
+    await driver.connect("wss://chat.edu.chicaga.ru");
+    await driver.user.login({
       login: this.user.chat_auth_info.username,
-      password: this.user.chat_auth_info.password,
-      room: this.$route.params.id,
-      socketUrl: "wss://chat.edu.chicaga.ru/websocket/",
-      chatElem: this.$refs.chat
+      password: this.user.chat_auth_info.password
     });
+    let channelId = "NBrWc5LsqrhmaRNKb";
+    try {
+      driver.user.createChannel(channelId);
+    } catch (error) {
+      console.log(error);
+    }
+    await driver.subscribe([channelId]);
+    await driver.channels[channelId].sendMessage("tesst");
+    console.log(driver.channels);
+    await driver.channels[channelId].loadHistory(300);
   }
 };
 </script>
