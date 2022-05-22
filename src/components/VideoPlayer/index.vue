66e0b10a342634272468807c320b6e17ff5a6047
GizzaProgger <147rawil147@gmail.com>
2022-05-22 15:13:17 +0400

    добавил подложку под видео

--- src/components/VideoPlayer/index.vue
+++ src/components/VideoPlayer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="video-player-wrap">
-    <div ref="videoElemSlot" class="video-slot">
+    <div class="video-slot">
       <figure
         class="vidFrame"
         ref="vidFrame"
@@ -8,62 +8,65 @@
       >
         <slot name="videoSlot" class="fullscreen-video-block"></slot>
         <chat :roomId="roomId" ref="chat" v-if="fullscreenOn" />
-        <figcaption class="vidBar" v-if="active">
-          <div class="top">
-            <!--  ПРОГРЕССБАР ДЛЯ ВИДЕО -->
-            <!-- <span class="buffered" :class="{ redline: active }"></span> -->
-            <Progress
-              @rewindTo="rewindTo"
-              ref="progress"
-              :currentTime="currentTime"
-              :duration="duration"
-              :timestamps="timestamps"
-            />
-          </div>
-          <div class="bottom">
-            <div class="left-side">
-              <play-svg :onPause="paused" @clickElem="togglePlay" />
-              <div
-                class="volume-area"
-                @mouseenter="showVolume"
-                @mouseleave="hideVolume"
-              >
-                <sound-svg :muted="muteVolume" @clickElem="toggleVolume" />
-                <div class="volume-input-block">
-                  <transition name="emersion">
-                    <input
-                      ref="volumeControl"
-                      v-show="volume"
-                      type="range"
-                      id="change_vol"
-                      v-model="changeVol"
-                      step="0.05"
-                      min="0"
-                      max="1"
-                      value="1"
-                    />
-                  </transition>
-                </div>
-              </div>
-              <div class="left-side__current-timestamp">{{ сurrentTitle }}</div>
-              <div class="left-side__current-time">
-                {{ currVideoTime }} / {{ formattedDuration }}
-              </div>
-            </div>
-            <div class="right-side">
-              <chat-svg
-                v-if="showChatButton"
-                :chatOff="chatState"
-                :fullscreenChatState="fullscreenChatState"
-                :fullScreenMode="fullscreenOn"
-                @clickElem="clickChat"
-              />
-              <expand-svg :expanded="fullscreenOn" @clickElem="clickExpand" />
-            </div>
-          </div>
-        </figcaption>
       </figure>
     </div>
+    <substrate :player-element="$el" :duration="1000">
+      <figcaption class="vidBar" v-if="active">
+        <div class="top">
+          <!--  ПРОГРЕССБАР ДЛЯ ВИДЕО -->
+          <Progress
+            @rewindTo="rewindTo"
+            ref="progress"
+            :currentTime="currentTime"
+            :duration="duration"
+            :timestamps="timestamps"
+          />
+        </div>
+        <div class="bottom">
+          <div class="left-side">
+            <play-svg :onPause="paused" @clickElem="togglePlay" />
+            <div
+              class="volume-area"
+              @mouseenter="showVolume"
+              @mouseleave="hideVolume"
+            >
+              <sound-svg :muted="muteVolume" @clickElem="toggleVolume" />
+              <div class="volume-input-block">
+                <transition name="emersion">
+                  <input
+                    ref="volumeControl"
+                    v-show="volume"
+                    type="range"
+                    id="change_vol"
+                    v-model="changeVol"
+                    step="0.05"
+                    min="0"
+                    max="1"
+                    value="1"
+                  />
+                </transition>
+              </div>
+            </div>
+            <div class="left-side__current-timestamp">
+              {{ сurrentTitle }}
+            </div>
+            <div class="left-side__current-time">
+              {{ currVideoTime }} / {{ formattedDuration }}
+            </div>
+          </div>
+          <div class="right-side">
+            <chat-svg
+              v-if="showChatButton"
+              :chatOff="chatState"
+              :fullscreenChatState="fullscreenChatState"
+              :fullScreenMode="fullscreenOn"
+              @clickElem="clickChat"
+            />
+            <expand-svg :expanded="fullscreenOn" @clickElem="clickExpand" />
+          </div>
+        </div>
+      </figcaption>
+    </substrate>
   </div>
 </template>
 
@@ -73,8 +76,9 @@ import SoundSvg from "@/components/Icons/SoundSvg";
 import ChatSvg from "@/components/Icons/ChatSvg";
 import ExpandSvg from "@/components/Icons/ExpandSvg";
 import Chat from "@/components/Chat/Chat";
-import Progress from "./Progress.vue";
+import Progress from "./Progress";
 import moment from "moment";
+import Substrate from "./Substrate";
 
 import api from "@/mixins/api";
 
@@ -86,7 +90,8 @@ export default {
     ChatSvg,
     ExpandSvg,
     Chat,
-    Progress
+    Progress,
+    Substrate
   },
   data() {
     return {
@@ -98,7 +103,7 @@ export default {
       volume: false,
       muteVolume: false,
       paused: false,
-      videoPlayer: undefined,
+      videoElement: undefined,
       fullscreenOn: false,
       fullscreenChatState: false,
       timestamps: []
@@ -112,7 +117,7 @@ export default {
   },
   computed: {
     videoIsActive() {
-      if (!this.videoPlayer) return false;
+      if (!this.videoElement) return false;
       return true;
     },
     roomId() {
@@ -127,7 +132,7 @@ export default {
   },
   methods: {
     onTimeUpdate() {
-      this.currentTime = this.videoPlayer.currentTime;
+      this.currentTime = this.videoElement.currentTime;
 
       for (let i = 0; i < this.timestamps.length; i++) {
         let previousTimeStampTime = i > 0 ? this.timestamps[i - 1].time : 0;
@@ -141,15 +146,15 @@ export default {
       }
     },
     rewindTo(x) {
-      this.videoPlayer.currentTime =
+      this.videoElement.currentTime =
         (x / this.$refs.progress.$el.clientWidth) * this.duration;
       this.onTimeUpdate();
     },
     playVideo() {
-      this.videoPlayer.play();
+      this.videoElement.play();
     },
     pauseVideo() {
-      this.videoPlayer.pause();
+      this.videoElement.pause();
     },
     showVolume() {
       this.volume = true;
@@ -167,13 +172,8 @@ export default {
       }
     },
     togglePlay() {
-      if (!this.paused) {
-        this.pauseVideo();
-        this.paused = true;
-      } else {
-        this.playVideo();
-        this.paused = false;
-      }
+      if (!this.paused) return this.pauseVideo();
+      return this.playVideo();
     },
     clickChat() {
       if (this.fullscreenOn)
@@ -218,33 +218,35 @@ export default {
   },
   watch: {
     changeVol() {
-      this.videoPlayer.volume = this.changeVol;
+      this.videoElement.volume = this.changeVol;
     }
   },
   mounted() {
     if (!this.$slots.videoSlot) return;
-    this.videoPlayer = this.$slots.videoSlot[0].elm.querySelector("video");
-    this.videoPlayer.volume = 0.5;
+    this.videoElement = this.$slots.videoSlot[0].elm.querySelector("video");
+    this.videoElement.volume = 0.5;
 
     if (this.isMobileSafari()) {
       // Hacks for Mobile Safari
-      this.videoPlayer.setAttribute("playsinline", true);
-      this.videoPlayer.setAttribute("controls", true);
+      this.videoElement.setAttribute("playsinline", true);
+      this.videoElement.setAttribute("controls", true);
       setTimeout(() => {
-        this.videoPlayer.removeAttribute("controls");
+        this.videoElement.removeAttribute("controls");
       });
     }
 
-    this.paused = this.videoPlayer.paused;
-    this.videoPlayer.addEventListener(
+    this.paused = this.videoElement.paused;
+    this.videoElement.addEventListener(
       "pause",
-      () => (this.paused = this.videoPlayer.paused)
+      () => (this.paused = this.videoElement.paused)
     );
-    this.videoPlayer.addEventListener("loadedmetadata", event => {
+    this.videoElement.addEventListener("loadedmetadata", event => {
       this.duration = event.target.duration;
       this.setTimestaps();
     });
-    this.videoPlayer.addEventListener("timeupdate", this.onTimeUpdate);
+    this.videoElement.addEventListener("timeupdate", this.onTimeUpdate);
+    this.videoElement.addEventListener("play", () => (this.paused = false));
+    this.videoElement.addEventListener("pause", () => (this.paused = true));
   }
 };
 </script>
@@ -264,106 +266,107 @@ export default {
     display: flex
     align-items: center
     justify-content: center
-    .vidBar
-      position: absolute
-      bottom: 0
-      right: 0
-      left: 0
-      padding: 15px 25px
+.vidBar
+  position: absolute
+  bottom: 0
+  right: 0
+  left: 0
+  padding: 15px 0
+  width: 100%
+  display: flex
+  flex-direction: column
+  .top
+    margin-bottom: .65rem
+    .buffered.redline
+      margin-bottom: 8px
+      background: #FF0000
+      height: 3px
       width: 100%
+      display: block
+  .bottom
+    display: flex
+    align-items: center
+    justify-content: space-between
+    padding: 0 15px
+    .left-side
       display: flex
-      flex-direction: column
-      .top
-        margin-bottom: .65rem
-        .buffered.redline
-          margin-bottom: 8px
-          background: #FF0000
-          height: 3px
-          width: 100%
-          display: block
-      .bottom
+      align-items: center
+      font-size: 13px
+      color: #ffffff
+      .play-svg
+        cursor: pointer
+      .volume-area
         display: flex
-        align-items: center
-        justify-content: space-between
-        .left-side
+        margin-left: 20px
+        .sound-svg
+          cursor: pointer
+        .volume-input-block
+          margin-left: 5px
+          overflow: hidden
           display: flex
           align-items: center
-          font-size: 13px
-          color: #ffffff
-          .play-svg
-            cursor: pointer
-          .volume-area
-            display: flex
-            margin-left: 20px
-            .sound-svg
-              cursor: pointer
-            .volume-input-block
-              margin-left: 5px
-              overflow: hidden
-              display: flex
-              align-items: center
-              .emersion-enter
-                transform: translateX(-129px)
-              .emersion-enter-active,
-              .emersion-leave-active
-                transition: all .2s ease-in-out
-              .emersion-enter-from,
-              .emersion-leave-to
-                transform: translateX(-129px)
-          .left-side__current-timestamp
-            margin-left: .5rem
-          .left-side__current-time
-            margin-left: .5rem
-        .right-side
-          display: flex
-          align-items: center
-          .expand-svg
-            margin-left: 18px
-        input[type=range]
-          -webkit-appearance: none
-          width: 50px
-          background: transparent
-          &:focus
-            outline: none
-          &::-webkit-slider-thumb
-            -webkit-appearance: none
-            border: none
-            height: 6px
-            width: 6px
-            border-radius: 50%
-            background: #FFFFFF
-            cursor: pointer
-            margin-top: -2px
-          &::-moz-slider-thumb
-            -webkit-appearance: none
-            border: none
-            height: 6px
-            width: 6px
-            border-radius: 50%
-            background: #FFFFFF
-            cursor: pointer
-            margin-top: -2px
-          &::-webkit-slider-runnable-track
-            width: 100%
-            height: 2px
-            border-radius: 1px
-            cursor: pointer
-            animate: 0.2s
-            background: #fff
-          &::-moz-range-track
-            width: 100%
-            height: 3px
-            border-radius: 1px
-            cursor: pointer
-            animate: 0.2s
-            background: #fff
-          &::-ms-track
-            width: 100%
-            height: 3px
-            border-radius: 1px
-            cursor: pointer
-            animate: 0.2s
-            background: #fff
+          .emersion-enter
+            transform: translateX(-129px)
+          .emersion-enter-active,
+          .emersion-leave-active
+            transition: all .2s ease-in-out
+          .emersion-enter-from,
+          .emersion-leave-to
+            transform: translateX(-129px)
+      .left-side__current-timestamp
+        margin-left: .5rem
+      .left-side__current-time
+        margin-left: .5rem
+    .right-side
+      display: flex
+      align-items: center
+      .expand-svg
+        margin-left: 18px
+    input[type=range]
+      -webkit-appearance: none
+      width: 50px
+      background: transparent
+      &:focus
+        outline: none
+      &::-webkit-slider-thumb
+        -webkit-appearance: none
+        border: none
+        height: 6px
+        width: 6px
+        border-radius: 50%
+        background: #FFFFFF
+        cursor: pointer
+        margin-top: -2px
+      &::-moz-slider-thumb
+        -webkit-appearance: none
+        border: none
+        height: 6px
+        width: 6px
+        border-radius: 50%
+        background: #FFFFFF
+        cursor: pointer
+        margin-top: -2px
+      &::-webkit-slider-runnable-track
+        width: 100%
+        height: 2px
+        border-radius: 1px
+        cursor: pointer
+        animate: 0.2s
+        background: #fff
+      &::-moz-range-track
+        width: 100%
+        height: 3px
+        border-radius: 1px
+        cursor: pointer
+        animate: 0.2s
+        background: #fff
+      &::-ms-track
+        width: 100%
+        height: 3px
+        border-radius: 1px
+        cursor: pointer
+        animate: 0.2s
+        background: #fff
 
 .video-chat-miniatures-wrapper
   .video-player-wrap
