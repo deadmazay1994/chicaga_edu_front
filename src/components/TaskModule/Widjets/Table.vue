067c684e7c4148aa4ad823c0048227592361315b
Anodoree <emfwws@gmail.com>
2022-05-24 11:41:36 +0300

    #178: Table.vue: fixes

--- src/components/TaskModule/Widjets/Table.vue
+++ src/components/TaskModule/Widjets/Table.vue
@@ -1,59 +1,43 @@
 <template>
-  <div class=' vue-component'>
-
-    <div class="d-table">
-      <div class="d-table__table">
-
-        <div class="d-table__head">
-
-          <div class="d-table__title">
-            food
-          </div>
-
-          <div class="d-table__title">
-            technology
-          </div>
-
-          <div class="d-table__title">
-            sports
-          </div>
-
-          <div class="d-table__title">
-            places
-          </div>
-
+  <div class="d-table vue-component">
+    <div class="d-table__table">
+      <div class="d-table__head">
+        <div class="d-table__title" style="width: 25%">
+          food
         </div>
-
-        <div class="d-table__body">
-
-          <div class="d-table__field">
-            gap
-          </div>
-          
-          <div class="d-table__field">
-            gap
-          </div>
-
-          <div class="d-table__field">
-            gap
-          </div>
-
-          <div class="d-table__field">
-            gap
-          </div>
-
+        <div class="d-table__title" style="width: 25%">
+          technology technology
+        </div>
+        <div class="d-table__title" style="width: 25%">
+          sports
+        </div>
+        <div class="d-table__title" style="width: 25%">
+          placesplacesplacesplaces
+        </div>
+      </div>
+      <div class="d-table__body">
+        <div class="d-table__field" style="width: 25%">
+          chipchipchipchipchipchipchipchipchipchip
+        </div>
+        <div class="d-table__field" style="width: 25%">
+          chip
+        </div>
+        <div class="d-table__field" style="width: 25%">
+          chip
+        </div>
+        <div class="d-table__field" style="width: 25%">
+          chip
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: "",
   data: function() {
-    return {}
+    return {};
   },
   methods: {},
   computed: {},
@@ -61,23 +45,25 @@ export default {
   props: [],
   mixins: {},
   beforeMount() {}
-}
+};
 </script>
 
-<style scoped='scoped' lang='sass'>
+<style scoped="scoped" lang="sass">
 .d-table
   width: 100%
   &__table
+    width: 100%
     display: flex
     flex-direction: column
     min-height: 240px
-  &__head
-    display: flex
-    align-items: center
-    max-width: 100%
     font-weight: 500
     font-size: 16px
     line-height: 20px
+    word-wrap: break-word
+  &__head
+    display: flex
+    align-items: center
+    width: 100%
     text-align: center
     color: #303030
     background: linear-gradient(180deg, #fff4f7 0%, #fcdbe3 100%)
@@ -85,19 +71,17 @@ export default {
     border-top-left-radius: 10px
   &__title
     flex-grow: 1
-    flex-basis: 100%
     padding: 10px
   &__body
     display: flex
     flex-grow: 1
-    max-width: 100%
+    width: 100%
     min-height: 200px
     border: 1px solid #e8e8e8
     border-bottom-right-radius: 10px
     border-bottom-left-radius: 10px
   &__field
     flex-grow: 1
-    flex-basis: 100%
     padding: 9px
   &__field:not(:last-child)
     border-right: 2px solid #e8e8e8
