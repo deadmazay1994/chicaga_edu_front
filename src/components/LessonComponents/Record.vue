c9fcbd30c7c258a230dbc6bf3feb36ef80f8a631
Evgeny <hatber.index@gmail.com>
2022-06-03 00:33:37 +0900

    добавлена страница просмотра товаров

--- src/components/LessonComponents/Record.vue
+++ src/components/LessonComponents/Record.vue
@@ -7,7 +7,7 @@
             ref="video"
             class="record__video"
             @click="$emit('click-by-video')"
-            src="https://chicaga-media.storage.yandexcloud.net/videos/example_lesson.mp4"
+            src="https://chicaga-media.storage.yandexcloud.net/videos/5 урок през.mp4"
           ></video>
         </div>
       </video-player>
