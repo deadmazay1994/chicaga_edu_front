652adc5f11507dc53f55843c33b0ee6fca51b271
GizzaProgger <147rawil147@gmail.com>
2022-06-05 13:03:26 +0400

    refactoring

--- src/components/LessonComponents/Record.vue
+++ src/components/LessonComponents/Record.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="lesson-left-side-component vue-component">
+  <div class="record vue-component">
     <video-player :active="true" ref="videoPlayer">
-      <div slot="videoSlot" class="videoSlot-block">
+      <div slot="videoSlot">
         <video
           ref="video"
-          class="video-component__video"
+          class="record__video"
           @click="$emit('click-by-video')"
           src="https://chicaga-media.storage.yandexcloud.net/videos/example_lesson.mp4"
         ></video>
       </div>
     </video-player>
-    <div class="lesson-left-side-component__bottom">
-      <timecodes @clickTimecode="clickTimecode" />
-      <evaluation />
-      <Textarea />
+    <div class="record__bottom bottom">
+      <timecodes class="bottom__timecodes" @clickTimecode="clickTimecode" />
+      <evaluation class="bottom__evaluation" />
+      <div class="bottom__text-review text-review">
+        <textarea-c class="text-review__textarea" placeholder="Оставьте отзыв к уроку" />
+        <svg class="text-review__icon">
+          <use xlink:href="#forward"></use>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -22,15 +27,15 @@
 import VideoPlayer from "@/components/VideoPlayer";
 import Timecodes from "@/components/VideoPlayer/Timecodes.vue";
 import Evaluation from "@/components/LessonComponents/Evaluation";
-import Textarea from "@/components/Chat/Textarea";
+import Textarea from "@/components/UiElements/InlineTextarea";
 
 export default {
-  name: "lesson-left-side-component",
+  name: "record",
   components: {
     VideoPlayer,
     Timecodes,
     Evaluation,
-    Textarea
+    'textarea-c': Textarea
   },
   data: function() {
     return {};
@@ -46,14 +51,29 @@ export default {
 </script>
 
 <style scoped="scoped" lang="sass">
-.lesson-left-side-component
+.record
   display: flex
   flex-direction: column
-  .lesson-left-side-component__bottom
+  &__bottom
+    padding: 0 24px
     margin-top: 20px
     display: flex
     flex-direction: column
     justify-content: space-between
     flex-grow: 1
     overflow-y: auto
+.bottom
+  &__timecodes
+    margin-bottom: 40px
+  &__evaluation
+    margin-bottom: 24px
+.text-review
+  display: flex
+  align-items: center
+  justify-content: space-between
+  &__textarea
+    width: 90%
+  &__icon
+    width: 24px
+    height: 24px
 </style>
---
+++ src/components/UiElements/InlineTextarea.vue
@@ -0,0 +1,34 @@
+<template>
+  <textarea
+    :placeholder="placeholder"
+    class="inline-textarea"
+    v-on:keyup.enter.exact="enter()"
+    @input="$emit('input', $e)"
+    rows="1"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: 'inline-textarea',
+  data: function() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  components: {},
+  props: ['placeholder'],
+  mixins: {},
+  beforeMount() {}
+}
+</script>
+
+<style scoped='scoped' lang='sass'>
+.inline-textarea
+  font-family: Manrope, sans-serif
+  font-weight: 500
+  padding: 7px 21px 7px 16px
+  background: hsla(0, 0%, 50%, .1)
+  border-radius: 10px
+  border: 0
+</style>
