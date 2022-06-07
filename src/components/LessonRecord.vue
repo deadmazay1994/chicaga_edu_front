8d2392b14bf45a3d17b01a9cea0c750b3df3c8fc
Anodoree <emfwws@gmail.com>
2022-06-07 23:13:02 +0300

    LessonRecord.vue: html & css рефактор

--- src/components/LessonRecord.vue
+++ src/components/LessonRecord.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class='lesson-template lessons-pg vue-component'>
+  <div class="lesson-template vue-component">
     <div class="lesson-template__header">
-      <Breadcrumb></Breadcrumb>
-      <scrores />
+      <div class="lesson-template__header-inner">
+        <div class="lesson-template__header-part">
+          <Breadcrumb></Breadcrumb>
+        </div>
+        <div class="lesson-template__header-part">
+          <scrores class="lesson-template__scores" />
+        </div>
+      </div>
     </div>
-    <div class="lesson-template__body lesson-body">
-      <record class="lesson-body__item lesson-body__video" />
-      <edu-panel class="lesson-body__item lesson__edu-panel lesson__item" />
+    <div class="lesson-template__body">
+      <div class="lesson-body">
+        <div class="lesson-body__inner">
+          <div class="lesson-body__item">
+            <record class="lesson-body__video" />
+          </div>
+          <div class="lesson-body__item lesson-body__item--2">
+            <edu-panel class="lesson-body__edu-panel" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Breadcrumb from "@/components/Base/BreadcrumbsComponent"
-import Scrores from "@/components/LessonComponents/Scores"
-import Record from "@/components/LessonComponents/Record"
-import EduPanel from "@/components/LessonComponents/EduPanel"
+import Breadcrumb from "@/components/Base/BreadcrumbsComponent";
+import Scrores from "@/components/LessonComponents/Scores";
+import Record from "@/components/LessonComponents/Record";
+import EduPanel from "@/components/LessonComponents/EduPanel";
 
 export default {
-  name: 'lesson-template',
+  name: "lesson-template",
   data: function() {
-    return {}
+    return {};
   },
   methods: {},
   computed: {},
@@ -33,27 +47,58 @@ export default {
   props: [],
   mixins: {},
   beforeMount() {}
-}
+};
 </script>
 
-<style scoped='scoped' lang='sass'>
+<style scoped="scoped" lang="sass">
+$header-height: 62px
+
 .lesson-template
-  overflow: hidden
   display: flex
   flex-direction: column
+  width: 100%
+  height: 100%
+  padding: 16px 20px 20px
   &__header
-    margin-bottom: 16px
-    display: flex
-    justify-content: space-between 
-  &__body
-    flex-grow: 1
-    overflow: hidden
+    position: relative
+    width: 100%
+    height: $header-height
+    padding-bottom: 16px
+    z-index: 1
+  &__header-inner
     display: flex
     justify-content: space-between
+    height: 100%
+  &__header-part
+    width: 50%
+  &__scores
+    padding-left: 20px
+  &__body
+    flex-grow: 1
+    width: 100%
+    height: calc( 100% - #{$header-height} )
+
 .lesson-body
+  position: relative
+  width: 100%
+  height: 100%
+  &__inner
+    display: flex
+    width: 100%
+    height: 100%
   &__item
     width: 50%
-    max-height: 100%
+    height: 100%
+  &__item--2
+    padding-left: 20px
   &__video
-    width: calc(50% - 20px)
+    width: 100%
+    height: 100%
+    padding-bottom: 13px
+  &__edu-panel
+    width: 100%
+    height: 100%
+    border-radius: 20px
+    box-shadow: 0 4px 20px rgba(128, 128, 128, 0.2)
+    overflow: hidden
 </style>
