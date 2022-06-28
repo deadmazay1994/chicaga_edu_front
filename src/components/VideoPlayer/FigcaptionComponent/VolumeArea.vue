f7f051ad99ecade0ddd30a4948d9eeecbaeb5d48
Anodoree <emfwws@gmail.com>
2022-06-28 15:44:13 +0300

    VolumeArea: refactor css

--- src/components/VideoPlayer/FigcaptionComponent/VolumeArea.vue
+++ src/components/VideoPlayer/FigcaptionComponent/VolumeArea.vue
@@ -4,8 +4,8 @@
     @mouseenter="$emit('mouseenter')"
     @mouseleave="$emit('mouseleave')"
   >
-    <sound-svg-vue :muted="muteVolume" @click="$emit('clickToggleVolume')" />
-    <div class="volume-input-block">
+    <sound-svg-vue class="volume-area__sound-svg" :muted="muteVolume" @click="$emit('clickToggleVolume')" />
+    <div class="volume-area__input-box">
       <transition name="emersion">
         <input
           type="range"
@@ -50,4 +50,23 @@ export default {
 };
 </script>
 
-<style scoped="scoped" lang="sass"></style>
+<style scoped="scoped" lang="sass">
+.volume-area
+  display: flex
+  &__sound-svg
+    cursor: pointer
+  &__input-box
+    display: flex
+    align-items: center
+    margin-left: 5px
+    overflow: hidden
+
+    .emersion-enter
+      transform: translateX(-129px)
+    .emersion-enter-active,
+    .emersion-leave-active
+      transition: all 0.2s ease-in-out
+    .emersion-enter-from,
+    .emersion-leave-to
+      transform: translateX(-129px)
+</style>
