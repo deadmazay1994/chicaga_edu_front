38b67d23a4bce1d68f621cb2b165a671e5d799f7
Anodoree <emfwws@gmail.com>
2022-06-29 00:12:05 +0300

    Stand, Record: отображение VideoRecord с таймкодами и без

--- src/components/LessonComponents/Record.vue
+++ src/components/LessonComponents/Record.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="record vue-component">
     <video-player :active="true" ref="videoPlayer" :timestamps="timestamps">
-      <div slot="videoSlot" class="video-slot">
+      <template slot="videoSlot">
         <video
           ref="video"
-          class="record__video"
           @click="$emit('click-by-video')"
-          src="https://chicaga-media.storage.yandexcloud.net/videos/5%20%D1%83%D1%80%D0%BE%D0%BA%20%D0%BF%D1%80%D0%B5%D0%B7.mp4"
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
         ></video>
-      </div>
+      </template>
     </video-player>
     <div class="record__bottom bottom">
       <timecodes
@@ -96,7 +95,4 @@ export default {
   &__icon
     width: 24px
     height: 24px
-
-.video-slot
-  background-color: #000000
 </style>
