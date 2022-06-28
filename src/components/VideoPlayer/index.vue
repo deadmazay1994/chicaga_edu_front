066eb755d981371370d76537bda2e1213e1243c1
Anodoree <emfwws@gmail.com>
2022-06-29 00:07:02 +0300

    VideoPlayer: refactor css - step 1

--- src/components/VideoPlayer/index.vue
+++ src/components/VideoPlayer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="video-player-wrap"
+    class="video-player"
     tabindex="0"
     ref="videoWrapper"
     @click="
@@ -11,29 +11,33 @@
     v-on:keyup.right="rewindToLeftOrRight(true)"
     v-on:keyup.left="rewindToLeftOrRight(false)"
   >
-    <div class="video-slot">
+    <div class="video-player__slot-wrap">
       <figure
-        class="vidFrame"
+        class="video-player__frame"
         ref="vidFrame"
         :class="{ chatActive: fullscreenChatState }"
       >
-        <slot name="videoSlot" class="fullscreen-video-block"></slot>
+        <slot name="videoSlot"></slot>
         <chat :roomId="roomId" ref="chat" v-if="fullscreenOn" />
 
         <rewind-animation-vue
-          class="show-rewind-svgs"
+          class="video-player__rewind-svgs"
           :hasRewindLeft="hasRewindLeft"
           :hasRewindRight="hasRewindRight"
         />
         <play-pause-animation-vue
+          class="video-player__play-pause"
           :videoJustPlayed="videoJustPlayed"
           :videoJustPaused="videoJustPaused"
         />
       </figure>
     </div>
-    <substrate :player-element="$el" style="z-index: 2" :duration="1000">
+    <substrate
+      class="video-player__substrate"
+      :player-element="$el"
+      style="z-index: 2"
+      :duration="1000">
       <figcaption-component
-        class="vidBar"
         ref="figcaption"
         :currentTime="currentTime"
         :duration="duration"
@@ -282,7 +286,7 @@ export default {
   },
   mounted() {
     if (!this.$slots.videoSlot) return;
-    this.videoElement = this.$slots.videoSlot[0].elm.querySelector("video");
+    this.videoElement = this.$slots.videoSlot[0].elm;
     this.videoElement.volume = 0.5;
 
     if (this.isMobileSafari()) {
@@ -314,199 +318,63 @@ export default {
 </script>
 
 <style lang="sass">
-.video-player-wrap
+.video-player
   position: relative
   border-radius: 20px
+  outline-style: none
   overflow: hidden
-  &__timecodes
-    margin-top: 21px
-    margin-left: 24px
-  .vidFrame
+  &__frame
+    position: relative
+    display: flex
+    align-items: center
+    justify-content: center
+    width: 100%
+    height: 100%
+  &__frame::before
+    content: ""
+    position: absolute
+    top: 1px
+    right: 1px
+    bottom: 1px
+    left: 1px
+    background-color: #000000
+    border-radius: 20px
+    z-index: -1
+  &__frame video
+    width: 100%
+    height: 100%
+    pointer-events: none
+  &__play-pause
+    position: absolute
+  &__rewind-svgs
+    position: absolute
+    display: flex
+    width: 100%
+    height: 100%
+  &__substrate
+    position: absolute
     top: 0
-    left: 0
-    width: 100%
-    display: flex
-    align-items: center
-    justify-content: center
-.vidBar
-  z-index: 3
-  position: absolute
-  bottom: 0
-  right: 0
-  left: 0
-  padding: 15px 0
-  width: 100%
-  display: flex
-  flex-direction: column
-  .top
-    margin-bottom: .65rem
-    .buffered.redline
-      margin-bottom: 8px
-      background: #FF0000
-      height: 3px
-      width: 100%
-      display: block
-  .bottom
-    display: flex
-    align-items: center
-    justify-content: space-between
-    padding: 0 15px
-    .left-side
-      display: flex
-      align-items: center
-      font-size: 13px
-      color: #ffffff
-      .play-svg
-        cursor: pointer
-      .volume-area
-        display: flex
-        margin-left: 20px
-        .sound-svg
-          cursor: pointer
-        .volume-input-block
-          margin-left: 5px
-          overflow: hidden
-          display: flex
-          align-items: center
-          .emersion-enter
-            transform: translateX(-129px)
-          .emersion-enter-active,
-          .emersion-leave-active
-            transition: all .2s ease-in-out
-          .emersion-enter-from,
-          .emersion-leave-to
-            transform: translateX(-129px)
-      .current-time
-        margin-left: .5rem
-      // .left-side__current-timestamp
-      //   margin-left: .5rem
-      // .left-side__current-time
-      //   margin-left: .5rem
-    .right-side
-      display: flex
-      align-items: center
-      .expand-svg
-        margin-left: 18px
-
-      &__settings
-        position: relative
-
-      .settings-menu-wrapper
-        position: absolute
-        // top: -175px
-        // left: -254px
-        transform: translate(-100%, -100%)
-    input[type=range]
-      -webkit-appearance: none
-      width: 50px
-      background: transparent
-      &:focus
-        outline: none
-      &::-webkit-slider-thumb
-        -webkit-appearance: none
-        border: none
-        height: 6px
-        width: 6px
-        border-radius: 50%
-        background: #FFFFFF
-        cursor: pointer
-        margin-top: -2px
-      &::-moz-slider-thumb
-        -webkit-appearance: none
-        border: none
-        height: 6px
-        width: 6px
-        border-radius: 50%
-        background: #FFFFFF
-        cursor: pointer
-        margin-top: -2px
-      &::-webkit-slider-runnable-track
-        width: 100%
-        height: 2px
-        border-radius: 1px
-        cursor: pointer
-        animate: 0.2s
-        background: #fff
-      &::-moz-range-track
-        width: 100%
-        height: 3px
-        border-radius: 1px
-        cursor: pointer
-        animate: 0.2s
-        background: #fff
-      &::-ms-track
-        width: 100%
-        height: 3px
-        border-radius: 1px
-        cursor: pointer
-        animate: 0.2s
-        background: #fff
-
-.video-chat-miniatures-wrapper
-  .video-player-wrap
-    height: 100%
-  .video-slot
-    height: 100%
-  .vidFrame
-    height: 100%
-
-.vidFrame:fullscreen
-  .lessons__messages
-    position: fixed
     right: 0
-    left: auto
-    width: 0%
-    opacity: 0
-    transition: .3s ease-in-out
-  .videoSlot-block,
-  .vidBar
     width: 100%
-    transition: .3s ease-in-out
-  &.chatActive
+    height: 100%
+
+  &__frame:fullscreen
     .lessons__messages
-      width: 30%
-      opacity: 1
-    .videoSlot-block,
-    .vidBar
-      width: 70%
-
-.videoSlot-block
-  width: 100%
-
-video
-  height: 100%
-  width: 100%
-  pointer-events: none
-
-.show-rewind-svgs
-  position: absolute
-  display: flex
-  height: 100%
-  width: 100%
-
-  &__elem
-    display: flex
-    align-items: center
-    justify-content: center
-    flex: 1
-
-.mobile-rewind-block
-  display: none
-  position: absolute
-  height: 100%
-  width: 100%
-
-  @media (max-width: 1000px)
-    &
-      display: flex
-
-  &__elem
-    width: 100%
-    height: 85%
-    cursor: pointer
-
-.play-pause-animation
-  position: absolute
+      position: fixed
+      right: 0
+      left: auto
+      width: 0%
+      opacity: 0
+      transition: .3s ease-in-out
+    .figcaption-component
+      width: 100%
+      transition: .3s ease-in-out
+    &.chatActive
+      .lessons__messages
+        width: 30%
+        opacity: 1
+      .figcaption-component
+        width: 70%
 
 .fade-enter-active, .fade-leave-active
   transition: opacity 1s
