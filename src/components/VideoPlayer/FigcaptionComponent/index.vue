6fa239b3d939706d4d0b412816c4607a082f847c
Anodoree <emfwws@gmail.com>
2022-06-28 17:37:32 +0300

    FigcaptionComponent: refactor css

--- src/components/VideoPlayer/FigcaptionComponent/index.vue
+++ src/components/VideoPlayer/FigcaptionComponent/index.vue
@@ -1,7 +1,8 @@
 <template>
   <figcaption class="figcaption-component" @click="$emit('click')">
-    <div class="top">
+    <div class="figcaption-component__top">
       <Progress
+        class="figcaption-component__progress"
         @rewindTo="rewindTo"
         ref="progress"
         :currentTime="currentTime"
@@ -9,10 +10,15 @@
         :timestamps="timestamps"
       />
     </div>
-    <div class="bottom">
-      <div class="left-side">
-        <play-svg-vue :onPause="paused" @click="$emit('togglePlay')" />
+    <div class="figcaption-component__bottom">
+      <div class="figcaption-component__bottom-left">
+        <play-svg-vue
+          class="figcaption-component__play-svg"
+          :onPause="paused"
+          @click="$emit('togglePlay')"
+        />
         <volume-area-vue
+          class="figcaption-component__volume-area"
           :volumeBoolean="volumeBoolean"
           v-model="volumeInputValue"
           @clickToggleVolume="$emit('clickToggleVolume')"
@@ -20,21 +26,23 @@
           @mouseleave="volumeBoolean = false"
         />
         <current-time-output-vue
-          class="current-time"
+          class="figcaption-component__current-time"
           :currentTitle="currentTitle"
           :currentVideoTime="currentVideoTime"
           :formattedDuration="formattedDuration"
         />
       </div>
-      <div class="right-side" style="z-index: 100">
-        <div class="right-side__settings">
+      <div class="figcaption-component__bottom-right" style="z-index: 100">
+        <div class="figcaption-component__settings">
           <settings-menu-vue
+            class="figcaption-component__settings-menu"
             v-if="settingsMenu"
             @changeSpeed="$emit('changeSpeed', speed)"
           />
           <gear-vue @click="settingsMenu = !settingsMenu" />
         </div>
         <chat-svg-vue
+          class="figcaption-component__chat-svg"
           v-if="showChatButton"
           :chatOff="chatState"
           :fullscreenChatState="fullscreenChatState"
@@ -42,6 +50,7 @@
           @clickElem="$emit('clickChat')"
         />
         <expand-svg-vue
+          class="figcaption-component__expand-svg"
           :expanded="fullscreenOn"
           @click="$emit('toggleExpand')"
         />
@@ -118,4 +127,40 @@ export default {
 };
 </script>
 
-<style scoped="scoped" lang="sass"></style>
+<style scoped="scoped" lang="sass">
+.figcaption-component
+  display: flex
+  flex-direction: column
+  padding-top: 15px
+  padding-bottom: 15px
+  font-size: 13px
+  color: #ffffff
+  &__top
+    margin-bottom: 10px
+  &__bottom
+    display: flex
+    align-items: center
+    justify-content: space-between
+    padding-right: 15px
+    padding-left: 15px
+  &__bottom-left
+    display: flex
+    align-items: center
+    margin-right: 10px
+  &__play-svg
+    cursor: pointer
+  &__volume-area
+    margin-left: 20px
+  &__current-time
+    margin-left: 0.5em
+  &__bottom-right
+    display: flex
+    align-items: center
+  &__settings
+    position: relative
+  &__settings-menu
+    position: absolute
+    transform: translate(-100%, -100%)
+  &__expand-svg
+    margin-left: 18px
+</style>
