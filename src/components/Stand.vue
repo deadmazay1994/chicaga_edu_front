6719f55411ee5063adcc7784b79f47395ed19caa
Anodoree <emfwws@gmail.com>
2022-07-13 19:09:21 +0300

    Stand: DemoPage added

--- src/components/Stand.vue
+++ src/components/Stand.vue
@@ -1,40 +1,24 @@
 <template>
   <div class="stand vue-component">
-    <tariff
-      v-for="(tariff, index) in tariffes"
-      :key="index"
-      :tariffData="tariff"
-    />
+    <inner-item />
   </div>
 </template>
 
 <script>
-import api from "@/mixins/api";
-import Tariff from "./Tariff";
+import InnerItem from "@/components/Lk/Courses/DemoPage";
 export default {
   name: "stand",
   data: function() {
-    return {
-      tariffes: undefined
-    };
+    return {};
   },
-  methods: {
-    async setTariff() {
-      const result = await api.methods.getTariffData();
-      this.tariffes = result.tariffes;
-    }
-  },
+  methods: {},
   computed: {},
   components: {
-    Tariff
+    InnerItem
   },
-  beforeMount() {
-    this.setTariff();
-  }
+  beforeMount() {}
 };
 </script>
 
 <style scoped="scoped" lang="sass">
-.stand
-  height: 100% // без этого свойства не будет работать прокрутка внутри компонента
 </style>
