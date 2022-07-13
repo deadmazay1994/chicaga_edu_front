6abed641f0e4cc052b1af58a4f5bc2a3b5fca137
Anodoree <emfwws@gmail.com>
2022-07-13 19:07:37 +0300

    DemoPage added

---
+++ src/components/Lk/Courses/DemoPage/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo-page vue-component">
+    <demo-description />
+  </div>
+</template>
+
+<script>
+import DemoDescription from "./DemoDescription.vue";
+
+export default {
+  name: "demo-page",
+  components: {
+    DemoDescription
+  },
+  data: function() {
+    return {};
+  },
+  props: [],
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped="scoped" lang="sass">
+.demo-page
+  padding: 32px
+  font-weight: 400
+  font-size: 16px
+  line-height: 1.5
+  color: #323232
+</style>
