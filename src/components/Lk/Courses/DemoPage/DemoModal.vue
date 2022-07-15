77d3d8d51d246b8eb3a65bd62aee1d9fda07451a
Anodoree <emfwws@gmail.com>
2022-07-15 14:54:13 +0300

    DemoModal refactor

--- src/components/Lk/Courses/DemoPage/DemoModal.vue
+++ src/components/Lk/Courses/DemoPage/DemoModal.vue
@@ -1,31 +1,40 @@
 <template>
-  <div class="demo-modal vue-component">
-    <default-modal-vue
-      :title="'Мини-курс для путешествий'"
-      :showModal="showModal"
-      @close="$emit('close')"
-    >
-      <div class="demo-modal__content">
-        <img
-          src="@/assets/imgs/modal-image.png"
-          alt="Изображение мини-курса путешествий"
-        />
-        <div class="demo-modal__content-right">
-          <span class="demo-modal__text"
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque et odio sed est pellentesque gravida sit amet at
-            orci.</span
-          >
+  <default-modal-vue
+    :title="'Мини-курс для путешествий'"
+    :showModal="showModal"
+    @close="$emit('close')"
+  >
+    <div class="demo-modal vue-component">
+      <div class="demo-modal__body">
+        <div class="demo-modal__img-box">
+          <img
+            class="demo-modal__img"
+            src="@/assets/imgs/modal-image.png"
+            width="209"
+            height="302"
+            alt="Изображение мини-курса путешествий"
+          />
+        </div>
+        <div class="demo-modal__content">
+          <div class="demo-modal__text-box">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque et odio sed est pellentesque gravida sit amet at
+              orci.
+            </p>
+          </div>
           <div class="demo-modal__teacher-info">
             <div class="demo-modal__teacher-text">
-              <div class="demo-modal__teacher-title">Предподаватель</div>
+              <div class="demo-modal__teacher-title">Преподаватель</div>
               <div class="demo-modal__teacher-name">Валерия</div>
             </div>
             <div class="demo-modal__avatar-wrapper">
               <img
                 class="demo-modal__teacher-image"
                 src="@/assets/imgs/teacher-avatar.png"
-                alt="Изображение учителя"
+                width="70"
+                height="70"
+                alt="Фотография учителя"
               />
             </div>
           </div>
@@ -34,16 +43,22 @@
       <div class="demo-modal__price-wrapper">
         <div class="demo-modal__price-title">Стоимость мини курса:</div>
         <div class="demo-modal__price-block">
-          <div class="demo-modal__old-price">2700</div>
-          <div class="demo-modal__new-price">1200 ₽</div>
+          <div class="demo-modal__price-box demo-modal__price-box--old">
+            <span class="demo-modal__old-price">2700</span
+            ><span class="demo-modal__old-currency">₽</span>
+          </div>
+          <div class="demo-modal__price-box demo-modal__price-box--new">
+            <span class="demo-modal__new-price">1200</span>
+            <span class="demo-modal__new-currency">₽</span>
+          </div>
         </div>
       </div>
-    </default-modal-vue>
-  </div>
+    </div>
+  </default-modal-vue>
 </template>
 
 <script>
-import DefaultModalVue from "../../../Modals/DefaultModal.vue";
+import DefaultModalVue from "@/components/Modals/DefaultModal.vue";
 
 export default {
   name: "demo-modal",
@@ -63,89 +78,102 @@ export default {
 
 <style scoped="scoped" lang="sass">
 .demo-modal
-  &__content
+  &__body
     display: flex
     justify-content: space-between
-  &__content-right
-    padding-left: 8px
     padding-right: 30px
+    padding-left: 8px
+  &__img-box
+    flex-shrink: 0
+    width: 209px
+    height: 302px
+    margin-right: 8px
+  &__img
+    width: 100%
+    height: 100%
+    object-fit: contain
+  &__content
     display: flex
     flex-direction: column
     justify-content: space-between
+  &__text-box
+    padding-top: 18px
+    padding-bottom: 18px
+    font-weight: 400
+    font-size: 18px
+    line-height: 1.6
   &__teacher-info
     display: flex
     align-items: center
     justify-content: flex-end
+    padding-top: 10px
+    padding-bottom: 20px
+    line-height: 1.5
+    text-align: right
   &__teacher-text
     display: flex
     flex-direction: column
     padding-right: 16px
   &__teacher-title
-    font-family: Manrope
-    font-size: 16px
     font-weight: 200
-    line-height: 24px
-    letter-spacing: 0em
-    text-align: right
+    font-size: 16px
   &__teacher-name
-    font-family: Manrope
-    font-size: 18px
     font-weight: 700
-    line-height: 27px
-    letter-spacing: 0em
-    text-align: right
+    font-size: 18px
   &__avatar-wrapper
-    width: 70px
-    height: 70px
-    border-radius: 50%
     display: flex
     align-items: center
     justify-content: center
+    flex-shrink: 0
+    width: 70px
+    height: 70px
+    border-radius: 50%
   &__teacher-image
-    object-fit: contain
+    width: 100%
+    height: 100%
+    object-fit: cover
   &__price-wrapper
     display: flex
     align-items: center
     justify-content: space-between
-    padding-left: 30px
-    padding-right: 30px
-    padding-top: 24px
-    padding-bottom: .5rem
+    padding: 10px 30px 8px
+  &__price-title
+    margin-right: 10px
+    font-weight: 700
+    font-size: 18px
   &__price-block
     display: flex
     flex-direction: column
     align-items: flex-end
-  &__price-title
-    font-weight: 700
-    font-size: 18px
-    line-height: 27px
-  &__old-price
-    position: relative
-    display: inline
-    font-weight: 600
+    line-height: 1
+  &__price-box--old
     font-size: 20px
-    line-height: 20px
-    color: #C4C4C4
-    &:after
-      content: ""
-      width: 100%
-      height: 1px
-      background-color: #C4C4C4
-      position: absolute
-      top: 12px
-      left: 0
-  &__new-price
-    display: inline
+    color: #c4c4c4
+  &__price-box--new
     font-weight: 200
-    font-size: 36px
-    line-height: 36px
-    text-align: right
-    background: linear-gradient(0deg, #DC3337 0%, #BE1C21 100%), linear-gradient(0deg, #6D9AF8 10.09%, #487EF3 85.32%)
+    font-size: 40px
+    background-color: #be1c21
+    background-image: linear-gradient(0deg, #dc3337 0%, #be1c21 100%)
     -webkit-background-clip: text
     -webkit-text-fill-color: transparent
-  &__text
-    padding-top: 18px
+    background-clip: text
+    text-fill-color: transparent
+  &__old-price
+    position: relative
+    display: inline-block
+    font-weight: 600
+  &__old-price::after
+    content: ""
+    position: absolute
+    top: 0.3em
+    left: 0
+    width: 100%
+    height: 1px
+    background-color: #c4c4c4
+  &__old-currency
     font-weight: 400
-    font-size: 18px
-    line-height: 29.8px
+    font-size: 0.6em
+  &__new-currency
+    margin-left: 0.2em
+    font-size: 0.5em
 </style>
