f06764f9d45c369eb34d7d3b55014363e6cac8a9
Evgeny <hatber.index@gmail.com>
2022-07-15 12:17:08 +0900

    добавил событие на копку minicourse-card__more-text

--- src/components/Lk/Courses/MinicourseCard.vue
+++ src/components/Lk/Courses/MinicourseCard.vue
@@ -13,7 +13,11 @@
         </span>
       </div>
       <div class="minicourse-card__more">
-        <button class="minicourse-card__more-text" type="button">
+        <button
+          class="minicourse-card__more-text"
+          type="button"
+          @click="$emit('showModal')"
+        >
           Подробнее
         </button>
       </div>
