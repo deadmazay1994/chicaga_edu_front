e9757c5e1896f688c661f766b4021c4ed13697a9
Anodoree <emfwws@gmail.com>
2022-07-17 14:24:46 +0300

    ShopCart: refactor

--- src/components/Lk/Store/Cart/ShopCart.vue
+++ src/components/Lk/Store/Cart/ShopCart.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="shop-cart">
     <div class="shop-cart__header">
-      <h3 class="shop-cart__title">
+      <div class="shop-cart__title">
         Корзина <span class="shop-cart__number">{{ cartItemsLength }}</span>
-      </h3>
-      <svg class="shop-cart__svg">
+      </div>
+      <svg class="shop-cart__icon" width="20" height="20">
         <use xlink:href="#cart"></use>
       </svg>
     </div>
@@ -85,45 +85,37 @@ export default {
 .shop-cart
   display: flex
   flex-direction: column
-  // justify-content: space-between
-  background: #FFFFFF
-  box-shadow: 0px 4px 10px rgba(128, 128, 128, 0.2)
+  padding: 24px
+  background-color: #ffffff
   border-radius: 20px
-  padding: 25px
-  margin: 0 0 0 35px
-  @media ($media_xl)
-    margin: 50px 0 0 15px
-    padding: 20px 15px
-  @media ($media_md)
-    margin: 50px 0 0 10px
-    padding: 15px 5px 15px 10px
-  @media ($media_md2)
-    display: none
+  box-shadow: 0 4px 10px rgba(128, 128, 128, 0.2)
+  overflow: hidden
   &__header
     display: flex
     justify-content: space-between
-  &__svg
-    width: 20px
-    height: 20px
-    .cart__path
-      fill: $white
+    margin-bottom: 24px
+  &__title
+    margin-right: 20px
+    font-weight: 600
+    font-size: 20px
+    line-height: 1.3
   &__number
     border-left: 1.5px solid rgba($gray, 0.1)
     padding-left: 10px
     margin-left: 5px
+  &__icon
+    width: 20px
+    height: 20px
   &__content
-    padding-right: 5px
     flex-grow: 1
     max-height: 600px
-    overflow-y: auto
-    margin-top: 10px
     margin-bottom: 40px
-    &::-webkit-scrollbar
-      width: 5px
-    /* Handle */
-    &::-webkit-scrollbar-thumb
-      background: rgba(128, 128, 128, 0.1)
-      border-radius: 40px
+    padding-right: 5px
+    overflow: hidden auto
+  &__content::-webkit-scrollbar
+    width: 5px
+  &__content::-webkit-scrollbar-thumb
+    background-color: rgba(128, 128, 128, 0.1)
   &__item:not(:last-child)
     margin-bottom: 10px
 
