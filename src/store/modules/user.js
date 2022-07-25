179eab92c1ffd03471453087bc5068bbbfb3b530
Evgeny <hatber.index@gmail.com>
2022-07-25 19:39:22 +0900

    fix

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -155,7 +155,7 @@ export default {
     user: false,
     logined: false,
     token: localStorage.getItem("token") || false,
-    isDemo: false
+    isDemo: true
   },
   getters: {
     user: state => {
