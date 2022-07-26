e804d883ce84556830235b38e84dfe6e7d9e1b25
Anodoree <emfwws@gmail.com>
2022-07-26 11:19:57 +0300

    MobileMenu: new logo, new basic styles

--- src/components/Lk/MobileMenu.vue
+++ src/components/Lk/MobileMenu.vue
@@ -1,71 +1,46 @@
 <template>
   <div class="mobile-menu">
-    <div class="menu-btn" @click.prevent="openBurgerMenu">
-      <span></span>
-      <span></span>
-      <span></span>
+    <div class="mobile-menu__logo-box">
+      <img
+        class="mobile-menu__logo"
+        src="~@/assets/imgs/logo-online.svg"
+        width="149"
+        height="26"
+        alt="Логотип"
+      />
     </div>
-    <logo class="mobile-menu__logo" />
   </div>
 </template>
 
 <script>
-import Logo from "@/components/Icons/Logo";
-
 export default {
   name: "MobileMenu",
-  components: {
-    Logo
-  },
-  methods: {
-    openBurgerMenu() {
-      this.$emit("click");
-    }
-  }
+  components: {},
+  methods: {}
 };
 </script>
 
 <style lang="sass" scoped>
-@import "@/assets/styles/variables.sass"
- 
-.mobile-menu
-  display: none
-  background: #fff
-  padding: 15px
-  box-shadow: 0 4px 20px hsl(0deg 0% 50% / 20%)
-  position: fixed
-  top: 0
-  left: 0
-  width: 100%
-  z-index: 9999
-  @media ($media_lg2)
-    display: flex
-    align-items: center
-    justify-content: space-between
-  @media ($media_sm)
-    padding-top: 10px
-    padding-bottom: 10px
-  &__logo
-    @media ($media_sm)
-      width: 150px
-      height: 36px
 
-.menu-btn
+.mobile-menu
+  position: relative
   display: flex
   align-items: center
-  justify-content: center
-  flex-direction: column
-  width: 36px
-  height: 36px
-  margin-right: 15px
-
-  span
-    display: block
-    width: 22px
-    height: 3px
-    margin-bottom: 3.5px
-    position: relative
-    background: #323232
-    border-radius: 3px
-    z-index: 1
+  justify-content: space-between
+  background-color: #ffffff
+  box-shadow: 0 4px 20px rgba(128, 128, 128, 0.1)
+  &::before
+    content: ""
+    position: absolute
+    bottom: 0
+    left: 0
+    width: 100%
+    height: 1px
+    background-color: #e3e3e3
+  &__logo-box
+    display: flex
+    padding: 12px 10px
+  &__logo
+    width: 149px
+    height: 26px
 </style>
